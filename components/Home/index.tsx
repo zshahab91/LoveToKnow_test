612b6da37b82d2350f49a85d8781290e23898ad8
zahra shahab <zshahab91@gmail.com>
2021-09-06 15:11:05 +0430

    finished design

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -2,73 +2,27 @@ import React, { useEffect, useState } from 'react';
 import cs from 'classnames';
 import MainLayout from '@components/MainLayout';
 import s from './styles.module.scss';
-import { Button, Card, Col, message, Row, Upload } from 'antd';
+import { Button, Card, Col, Collapse, message, Row, Upload } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+
+interface FileObjectInterface {
+  sum: number;
+  valid_has_file: boolean;
+  name_has_file: string;
+  file_name: string;
+  contents: string[];
+}
 const HomePage = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<FileObjectInterface[]>([]);
   const [urls, setUrls] = useState([]);
   const [finished, setFinished] = useState(false);
   const [loadings, setLoadings] = useState(false);
-  const handleFileReadOld = async (content, file_name) => {
-    let arrayContent = content.split('\n');
-    let has_file,
-      final_obj = null;
-    let is_txt_index = arrayContent.findIndex((element) => element.includes('.txt'));
-    console.group('file');
-    console.log('file_name', file_name);
-    console.log('arrayContent', arrayContent);
-    console.log('is_txt_index', is_txt_index);
-    console.groupEnd();
-    if (is_txt_index !== -1) {
-      const item = arrayContent[is_txt_index];
-      has_file = item;
-      let file = urls.find((url) => url.name === item);
-      if (file) {
-        new Blob([file]).text().then(async (x) => {
-          const res = await handleFileRead(x, item);
-          console.log('res', res);
-          if (res) {
-            arrayContent[is_txt_index] = res?.sum;
-            final_obj = {
-              sum: arrayContent.reduce((partial_sum, a) => parseFloat(partial_sum) + parseFloat(a), 0),
-              has_file: has_file,
-              file_name: file_name,
-            };
-            console.log('final_obj has file is:', final_obj);
-
-            setData((data) => [...data, final_obj]);
-            return final_obj;
-          }
-        });
-      } else {
-        final_obj = {
-          sum: arrayContent.reduce(
-            (partial_sum, a) => parseFloat(partial_sum) + (Number.isNaN(parseFloat(a)) ? 0 : parseFloat(a)),
-            0
-          ),
-          has_file: has_file,
-          file_name: file_name,
-        };
-        console.log('final_obj is not file:', final_obj);
-        setData((data) => [...data, final_obj]);
-        return final_obj;
-      }
-    }
-    final_obj = {
-      sum: arrayContent.reduce(
-        (partial_sum, a) => parseFloat(partial_sum) + (Number.isNaN(parseFloat(a)) ? 0 : parseFloat(a)),
-        0
-      ),
-      has_file: has_file,
-      file_name: file_name,
-    };
-    return final_obj;
-  };
+  const [showContents, setShowContents] = useState<string[]>([]);
   const handleFileRead = async (content, file_name) => {
     let arrayContent = content.split('\n');
     let has_file,
       file,
-      final_obj = null;
+      final_obj: FileObjectInterface = null;
     let is_txt_index = arrayContent.findIndex((element) => element.includes('.txt'));
     if (is_txt_index !== -1) {
       const item = arrayContent[is_txt_index];
@@ -87,6 +41,7 @@ const HomePage = () => {
       valid_has_file: !!file,
       name_has_file: has_file,
       file_name: file_name,
+      contents: arrayContent,
     };
     setData((data) => [...data, final_obj]);
     setFinished(!file);
@@ -101,22 +56,13 @@ const HomePage = () => {
         currentObj = item;
       }
     }
-    setData(oldData);
+    setData(oldData.reverse());
     setLoadings(false);
   };
-  // const readyContent = async (file) => {
-  //   URL.createObjectURL(file);
-  //   fileReader = new FileReader();
-  //   fileReader.readAsText(file);
-  //   fileReader.onloadend = handleFileRead;
-  // };
 
   const handleFileChosen = ({ file, fileList }) => {
-    console.log('file', fileList);
     if (file.status !== 'uploading') {
-      // for (const file of fileList) {
       setUrls(fileList);
-      // }
     }
     if (file.status === 'done') {
       message.success(`${file.name} file uploaded successfully`);
@@ -124,20 +70,8 @@ const HomePage = () => {
       message.error(`${file.name} file upload failed.`);
     }
   };
-  // const test = async (data) => {
-  //   console.log('data', data);
-  //   for (const i in data) {
-  //     if (typeof data[i] !== 'number') {
-  //       const t = await test(data[i]);
-  //       data[i] = t;
-  //       return data.reduce((target, item) => item + target, 0);
-  //     }
-  //   }
-  //   return data.reduce((target, item) => item + target, 0);
-  // };
+
   const startProgress = async () => {
-    //  const r = await test([1, 2, 3, [5, 7, [6, 10, 2, [3, 0]]], 1]);
-    //  console.log('r', r);
     setLoadings(true);
     setTimeout(() => {
       new Blob([urls[0].originFileObj]).text().then(async (x) => {
@@ -150,22 +84,21 @@ const HomePage = () => {
       calcSum();
     }
   }, [finished]);
-  // useEffect(() => {
-  //   if (urls.length > 0) {
-  //     new Blob([urls[0]]).text().then(async (x) => {
-  //       startProgress(urls[0]);
-  //     });
-  //   }
-  // }, [urls]);
-  console.log('allData', data);
   return (
     <div className="container-fluid no_padding">
       <MainLayout title="main-page">
         <div className={cs('container', s.main_page)}>
           <div className="row">
             <div className="col-12">
-              <Card title="Please Upload your files" style={{ width: '50%', margin: '10px auto', textAlign: 'center' }}>
-                <Upload multiple accept=".txt" onChange={(e) => handleFileChosen(e)}>
+              <Card title="Please Upload your files" className={s.upload}>
+                <Upload
+                  progress={{ strokeWidth: 2, showInfo: true }}
+                  multiple
+                  showUploadList={{ showRemoveIcon: true }}
+                  accept=".txt"
+                  onRemove={(e) => setUrls(urls.filter((item) => item.name !== e.name))}
+                  onChange={(e) => handleFileChosen(e)}
+                >
                   <Button icon={<UploadOutlined />}>Upload</Button>
                 </Upload>
                 <Button
@@ -173,6 +106,7 @@ const HomePage = () => {
                   style={{ margin: '20px auto' }}
                   loading={loadings}
                   onClick={() => startProgress()}
+                  disabled={urls.length === 0}
                 >
                   Start Read Files
                 </Button>
@@ -180,8 +114,8 @@ const HomePage = () => {
               <Row gutter={16}>
                 {data.map((file, inx) => {
                   return (
-                    <Col span={8}>
-                      <Card title={file.file_name} bordered={false} key={inx}>
+                    <Col span={8} key={inx}>
+                      <Card title={file.file_name} bordered={false} className={cs(s.card)}>
                         Sum of this file is: <b>{file.sum}</b>
                         {file.name_has_file && (
                           <>
@@ -191,23 +125,29 @@ const HomePage = () => {
                             </p>
                           </>
                         )}
+                        <Collapse
+                          className={s.content}
+                          onChange={() =>
+                            showContents.includes(file.file_name)
+                              ? setShowContents(showContents.filter((item) => item !== file.file_name))
+                              : setShowContents([...showContents, file.file_name])
+                          }
+                        >
+                          <Collapse.Panel
+                            header={showContents.includes(file.file_name) ? 'Hide Content' : 'Show Content'}
+                            key={inx}
+                          >
+                            {showContents.includes(file.file_name) &&
+                              file.contents.map((content, index) => {
+                                return <p key={index}>{content}</p>;
+                              })}
+                          </Collapse.Panel>
+                        </Collapse>
                       </Card>
                     </Col>
                   );
                 })}
               </Row>
-              {/* <div className="upload-expense">
-                <label htmlFor="file">Please Upload your files:</label>
-                <input
-                  type="file"
-                  id="file"
-                  multiple
-                  className="input-file"
-                  accept=".txt"
-                  onChange={(e) => handleFileChosen(e.target.files)}
-                />
-                <button>Start Calc</button>
-              </div> */}
             </div>
           </div>
         </div>
